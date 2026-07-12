710a9f68e87b63b3bf015fa15d66b98133f903a3
Prashant Bhusal <parshantbhusal1928@gmail.com>
2026-07-12 13:28:31 +0545

    refactor: remove sessions and verification code from auth modules

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -7,6 +7,7 @@ import { errorHandler } from "./middlewares/error_middleware.js";
 import { requestLoggerMiddleware } from "./middlewares/request_logger_middleware.js";
 import authRouter from "./modules/auth/auth.route.js";
 import sessionRouter from "./modules/session/session.route.js";
+import verificationRouter from "./modules/verification/verification.route.js";
 import cookieParser from "cookie-parser";
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./config/swagger.js";
@@ -34,7 +35,7 @@ app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 }
 app.use("/api/auth", authRouter);
 app.use("/api/session", sessionRouter);
-
+app.use("/api/verification",verificationRouter)
  app.get("/",(req,res)=>{
   res.send("Welcome to the API server!")
  })
